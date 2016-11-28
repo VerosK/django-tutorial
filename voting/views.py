2abb42855314923e15f8965d5a5b3946227fda76
Veroslav Kaplan <xkaplan@fi.muni.cz>
2016-11-28 11:10:36 +0100

    Added .save & redirect

--- voting/views.py
+++ voting/views.py
@@ -1,9 +1,9 @@
 # voting/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import datetime                      
-from .models import Voting              
+from .models import Voting, Answer
 
 
 def index(req):
@@ -18,5 +18,9 @@ def one_poll(req, poll_id):
     )
     return render(req, 'voting/one_poll.html', context)
     
-
-    
+def add_vote(req, answer_id):
+    answer = Answer.objects.get(id=answer_id)
+    answer.count += 1
+    answer.save()
+    return_url =  '/{}/'.format(answer.voting.id)
+    return redirect(return_url)
--- voting_machine/urls.py
+++ voting_machine/urls.py
@@ -8,5 +8,6 @@ import voting.views
 urlpatterns = [
     url(r'^$', voting.views.index),        
     url(r'^(?P<poll_id>\d+)/$', voting.views.one_poll),
+    url(r'^vote/(?P<answer_id>\d+)/$', voting.views.add_vote),
     url(r'^admin/', admin.site.urls),
 ]
